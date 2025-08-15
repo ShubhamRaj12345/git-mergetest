071a86191cbdac3bd39c14b3f16dd9e3b4e70880
Sonu <sonu108rp@gmail.com>
2025-08-15 15:34:22 +0530

    form removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import { useState } from 'react'
+
 import './App.css'
 import './style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
         <h1>Enter Your Details</h1>
        <div className='io'>
 
-    <form>
-     <input type='text' placeholder="Enter Name"name="text"></input>
-     <br></br>
-      <input type='password' placeholder="Enter Password"name="text"></input>
-    <br></br>
-
-    <button>Submit</button>
-<br></br>
-    </form>
+        <div>
+          <p> as;dlkfjasdofsdjl;sadkjfasd;l   sdlkfjsdofsd d fldsfosd 
+          </p>
+        </div>
 
        </div>
     </>
